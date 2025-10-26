b868899489c9ec0d4a55ad6142e675ead32f43e2
morangoo <bms.santos@outlook.pt>
2025-10-26 12:10:58 +0000

    3Sum

---
+++ 3Sum/Solution.java
@@ -0,0 +1,32 @@
+import java.util.*; 
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        Arrays.sort(nums);
+        ArrayList<List<Integer>> result = new ArrayList<>(); 
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            while (left < right) {
+                int sum = nums[left] + nums[i] + nums[right]; 
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[left], nums[i], nums[right]));
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                }
+                else if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+}  
